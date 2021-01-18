5b9cf8fd9bf2e6920c751c86f5819e1e4a6e4dc1
khalidpark2029 <70706704+khalidpark@users.noreply.github.com>
2021-01-18 23:01:51 +0900

    Add files via upload

---
+++ 땅부자알리미/main.py
@@ -0,0 +1,9 @@
+#from indeed import get_jobs as get_indeed_jobs
+from land import extract_info
+#from save import save_to_file
+
+extract_info
+#indeed_jobs = get_indeed_jobs()
+#so_jobs = get_so_jobs()
+#jobs = so_jobs + indeed_jobs
+#save_to_file(jobs)
